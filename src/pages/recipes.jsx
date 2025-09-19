3a1a92664fc90306aa0032943ce340f8f08ff14a
congpx-udu <congpx.ptit@gmail.com>
2025-09-19 22:26:29 +0700

    demo sp

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -1,3 +1,16 @@
 export default function Recipes() {
-  return <section className="recipes" id="home"></section>;
+  return (
+    <section className="recipes" id="home">
+      <div className="menu-header">
+        <h1>Quay lại</h1>
+        <div className="backHome" onClick={() => window.history.back()}>
+          <i className="fa-solid fa-chevron-left"></i>
+        </div>
+        <div className="goHome" onClick={() => (window.location.href = "/")}>
+          🏠 Trang chủ
+        </div>
+      </div>
+      <h1>Trang web đang phát triển...</h1>
+    </section>
+  );
 }
